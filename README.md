6d35932c2212e529a2c853bb3417a87863342fff
Rollulus <roelboel@xs4all.nl>
2016-07-08 17:58:40 +0200

    Proves the concept

--- README.md
+++ README.md
@@ -3,3 +3,5 @@ Kafka Connect FTP
 
 Monitors files on an FTP server and feeds changes into Kafka.
 
+Status: WIP. Immature, naive, convoluted, happy path only.
+
--- src/main/scala/com/eneco/trading/kafka/connect/ftp/source/FtpSourceTask.scala
+++ src/main/scala/com/eneco/trading/kafka/connect/ftp/source/FtpSourceTask.scala
@@ -1,24 +1,60 @@
 package com.eneco.trading.kafka.connect.ftp.source
 
+import java.time.{Duration, Instant}
 import java.util
 
+import org.apache.kafka.connect.data.Schema
+import org.apache.kafka.connect.errors.ConnectException
 import org.apache.kafka.connect.source.{SourceRecord, SourceTask}
 
 import scala.collection.JavaConverters._
+import scala.util.{Failure, Success}
 
 class FtpSourceTask extends SourceTask with Logging {
+  var ftpMonitor: Option[FtpMonitor] = None
+  var lastPoll = Instant.EPOCH
+  val PollDuration = Duration.ofSeconds(10)
 
-  override def stop(): Unit = {
+  override def stop (): Unit = {
   }
 
-  override def start(map: util.Map[String, String]): Unit = {
+  override def start(props: util.Map[String, String]): Unit = {
+    val sourceConfig = new FtpSourceConfig(props)
+    ftpMonitor = Some(new FtpMonitor(sourceConfig.getString(FtpSourceConfig.Address),
+      sourceConfig.getString(FtpSourceConfig.User),
+      sourceConfig.getPassword(FtpSourceConfig.Password).value, Map[String, FileMetaData]()))
   }
 
   override def version(): String = getClass.getPackage.getImplementationVersion
 
   override def poll(): util.List[SourceRecord] = {
     log.info("poll")
-    Seq[SourceRecord]().asJava
+    ftpMonitor match {
+      case Some(ftp) if Instant.now.isAfter(lastPoll.plus(PollDuration)) =>
+        ftp.poll(
+          new MonitoredDirectory("/", ".*", true) :: Nil
+        ) match {
+          case Success((fileChanges, knownFiles)) =>
+            lastPoll = Instant.now
+            fileChanges.map { case (meta, body) =>
+              new SourceRecord(
+                Map("path" -> meta.attribs.path).asJava, // source part
+                Map("offset" -> body.offset).asJava, // source off
+                "ftp-hardcoded", //top
+                Schema.STRING_SCHEMA, // key sch
+                meta.attribs.path, // key
+                Schema.BYTES_SCHEMA, // val sch
+                body.bytes // val
+              )
+            }.asJava
+          case Failure(err) =>
+            Seq[SourceRecord]().asJava
+        }
+      case Some(ftp) =>
+        Thread.sleep(1000)
+        Seq[SourceRecord]().asJava
+      case None => throw new ConnectException("not initialised")
+    }
   }
 }
 
